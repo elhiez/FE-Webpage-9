e19e92d3928c11ca5ae68e4bd7f831b2f96f70cd
elhiez <ehgnedits@gmail.com>
2024-06-30 15:59:13 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,5 @@
+
+    document.getElementById('themeButton').addEventListener('click', function() {
+        document.body.classList.toggle('dark-theme')
+    });
+/* The code above functions as a dark/light mode toggle button with JavaScript */
